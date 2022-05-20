acf1dcc3a840b9f52cdc3add026e7f63bd7edf28
Clay Xiong <klayxi@gmail.com>
2022-05-20 17:04:27 +0900

    completed the styling for navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,8 +5,8 @@ import { faEarthAmericas } from "@fortawesome/free-solid-svg-icons";
 export default function Navbar() {
     return(
         <div className="Navbar">
-            <FontAwesomeIcon icon={faEarthAmericas} />
-            <h1>my travel journal</h1>
+            <FontAwesomeIcon icon={faEarthAmericas} className="globe-icon" />
+            <h1 className="navbar-title">my travel journal.</h1>
         </div>
     )
 }
