5ea0310053e1da971b4bae35f434fca55aeeecfc
Clay Xiong <klayxi@gmail.com>
2022-05-20 16:39:56 +0900

    completed LocationCard component

--- src/Components/LocationCard.js
+++ src/Components/LocationCard.js
@@ -1,10 +1,18 @@
 import React from "react";
 
 export default function LocationCard(props) {
-    return(
-        <div className="LocationCard">
-            <img src={props.imageUrl} />
-            <h2 classname="location-title">{props.title}</h2>
+  return (
+    <div className="LocationCard">
+      <img src={props.imageUrl} />
+      <div className="card-info">
+        <div className="location-container">
+          <p className="card-location">{props.location}</p>
+          <a href={props.googleMapsUrl}>View on Google Maps</a>
         </div>
-    )
+        <h2 classname="card-title">{props.title}</h2>
+        <p className="card-date">{`${props.startDate} - ${props.endDate}`}</p>
+        <p className="card-description">{props.description}</p>
+      </div>
+    </div>
+  );
 }
