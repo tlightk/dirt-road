f953dbed4fa15a02b84bed07062021b182e67969
Clay Xiong <klayxi@gmail.com>
2022-05-20 16:08:54 +0900

    created data.js and removed locations folder

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    title: "King's Landing",
+    location: "Westeros",
+    googleMapsUrl: "https://goo.gl/maps/g31Cmb5jwnQbSt9f9",
+    startDate: "1 Jun, 2022",
+    endDate: "24 Jun, 2022",
+    description:
+      "King's Landing is the capital, and largest city, of the Six Kingdoms. Located on the east coast of Westeros in the Crownlands, just north of where the Blackwater Rush flows into Blackwater Bay and overlooking Blackwater Bay, King's Landing is the site of the Iron Throne and the Red Keep, the seat of the King of the Andals and the First Men (hence the name).",
+    imageUrl: "https://assets.vogue.com/photos/598dacb5f0b0e21484d342ba/master/w_2560%2Cc_limit/00-lede-a-game-of-thrones-guide-to-dubrovnik-croatia.jpg",
+  },
+  {
+    title: "Kaer Morhen",
+    location: "Kaedwen",
+    googleMapsUrl: "https://goo.gl/maps/GTCJbfJrKtvoofbH9",
+    startDate: "12 Nov, 2022",
+    endDate: "30 Nov, 2022",
+    description:
+      `Kaer Morhen (corruption of Elder Speech: Caer a'Muirehen, meaning Keep of the Elder Sea) is an old keep where witchers of the School of the Wolf used to be trained. It is located in the mountains of Hertch in the kingdom of Kaedwen, right off the Gwenllech river. The castle can only be reached by "The Witchers Trail", nicknamed "The Killer", which is easy to miss and encircles the keep.`,
+    imageUrl: "https://goo.gl/maps/W8m63HigZCFbBPVs6",
+  },
+  {
+    title: "Mordor",
+    location: "Middle Earth",
+    googleMapsUrl: "https://goo.gl/maps/1fNdwKeSTYJyssM17",
+    startDate: "25 Dec, 2022",
+    endDate: "5 Jan, 2023",
+    description:
+      "Mordor was a black, volcanic plain in the southeast of Middle-earth to the east of Gondor, Ithilien, and the great river Anduin. Mordor was chosen by Sauron as his realm because of the mountain ranges surrounding it on three sides, creating a natural fortress against his enemies.",
+    imageUrl: "https://i.pinimg.com/originals/41/ef/e1/41efe19ee47ce115fbc4dcf37ed5cd3b.jpg",
+  },
+];
