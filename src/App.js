c873555d44a2cc2cd2e3425d4f4adf2e65a472b5
Clay Xiong <klayxi@gmail.com>
2022-05-20 16:21:31 +0900

    updated data.js to include id and created locationCards mapping variable

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import './App.css';
-import Navbar from './Components/Navbar';
-import LocationCard from './Components/LocationCard';
+import React from "react";
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import LocationCard from "./Components/LocationCard";
 import data from "./data";
 
 function App() {
+  const locationCards = data.map((location) => (
+    <LocationCard key={location.id} {...location} />
+  ));
 
   return (
     <div className="App">
       <Navbar />
       <LocationCard />
-
     </div>
   );
 }
