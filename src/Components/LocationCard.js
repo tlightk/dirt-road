8ac57a2a15bc617b5674e5e12771231122f64f67
Clay Xiong <klayxi@gmail.com>
2022-05-23 09:49:10 +0900

    completed styling for card component

--- src/Components/LocationCard.js
+++ src/Components/LocationCard.js
@@ -5,14 +5,14 @@ import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
 export default function LocationCard(props) {
   return (
     <div className="LocationCard">
-      <img src={props.imageUrl} className="location-image" />
+      <img src={props.imageUrl} className="location-image" alt="location"/>
       <div className="card-info">
         <div className="location-container">
           <FontAwesomeIcon icon={faLocationDot} className="marker-icon" />
           <p className="card-location">{props.location}</p>
-          <a href={props.googleMapsUrl}>View on Google Maps</a>
+          <a href={props.googleMapsUrl} className="maps-link">View on Google Maps</a>
         </div>
-        <h2 classname="card-title">{props.title}</h2>
+        <h2 className="card-title">{props.title}</h2>
         <p className="card-date">{`${props.startDate} - ${props.endDate}`}</p>
         <p className="card-description">{props.description}</p>
       </div>
